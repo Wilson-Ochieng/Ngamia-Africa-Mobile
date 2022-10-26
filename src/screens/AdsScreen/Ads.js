64543640d0f2820e599d89da250e47dbe26ccf5c
wilsonochieng718@gmail.com <wilsonochieng718@gmail.com>
2022-10-26 18:03:57 +0300

    Replaced AdvertsPage with Ads Page

--- src/screens/AdsScreen/Ads.js
+++ src/screens/AdsScreen/Ads.js
@@ -1,80 +1,48 @@
-import { View, Text,StyleSheet,ScrollView} from 'react-native'
-import React,{useState} from 'react'
+import { View, Text, Image, StyleSheet, useWindowDimensions, ScrollView } from 'react-native'
+import React, { useState } from 'react'
 import Input from '../../components/CustomInput/Input';
 import CustomButton from '../../components/CustomButton/CustomButton';
+import SocialSignins from '../../components/SocialSignins/SocialSignins';
 import { useNavigation } from '@react-navigation/native';
 
+
+
 const Ads = () => {
-    const [code, setCode] = useState('');
-    const navigation = useNavigation();
-   
-    //Height needed for logo Sign Screen only
-    const onConfirmPressed= () => {
-        navigation.navigate('Home')
-    }
 
-    
-    const onSigninPressed = () => {
 
-     navigation.navigate('Signin')
-        
-    }
-    const onResendPressed = () => {
-        
+    const [adverts, setAdverts] = useState([]);
+    const [loading, setLoading] = useState(true)
+    const url = "https://jsonplaceholder.typicode.com/photos";
+    useEffect(() => {
 
-    }
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => setAdverts(json))
+            .catch((error) => alert(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+
+
+
     return (
-        <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.root}>
-            <Text style={styles.title}> Confirm your email</Text>
-            <Input
-                placeholder="Enter your confirmation code"
-                value={code}
-                    setValue={setCode} />
+        <View >
+            {
+                loading ? <Text>Loading ...</Text> : (
+                    adverts.map((photo) => (
+                        <View>
+                            <Text> {photo.thumbnailUrl}</Text>
+                            <Text> {photo.albumId}</Text>
 
 
-            <CustomButton
-                text='Confirm'
-                onPress={onConfirmPressed} />
-                           
-            <CustomButton
-                text='Resend Code'
-                onPress={onResendPressed}
-                type="SECONDARY" />
-      
-            <CustomButton
-                text='Back to Sign in'
-                onPress={onSigninPressed}
-                type="TERTIARY" />
-      
-            </View>
-            </ScrollView>
+                        </View>
+                    )
+                    )
+                )
+            }
 
-        
-    );
-};
+        </View>
+    )
+}
 
-const styles = StyleSheet.create({
-    root: {
-        alignItems: "center",
-        padding:20,
-    },
-        
-    title: {
-        
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#051C60',
-        margin:10,
-    },
-    text: {
-        color: "gray",
-        marginVertical: 10,
-
-        
-    },
-    link: {
-        color:"#FDB075"
-    }
-});
 export default Ads
